a11907c3f64d8892d997980fbb0c0025e8d56bc2
colinvwood <woodcolin38@gmail.com>
2021-07-29 11:56:59 -0400

    template test

---
+++ site/test/example.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+const fileToTest = require('../fileToTest.js')
+
+describe('nameOfTest', function() {
+    it('function call should return x', function() {
+        assert.equal(funcCall(), expectedReturn)
+    }
+}
