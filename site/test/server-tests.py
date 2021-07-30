f93e627219f1465e3253c549f08ee7bf73d1d743
colinvwood <woodcolin38@gmail.com>
2021-07-30 00:03:07 -0400

    added server test python script

---
+++ site/test/server-tests.py
@@ -0,0 +1,26 @@
+import requests
+
+home_page = requests.get('https://www.colinwood.dev/alertx/', verify=False)
+if home_page.status_code == 200:
+    print("Passed")
+else:
+    print("Failed - home page down")
+
+products_page = requests.get('https://www.colinwood.dev/alertx/products/', verify=False)
+if products_page.status_code == 200:
+    print("Passed")
+else:
+    print("Failed - products page down")
+
+about_us_page = requests.get('https://www.colinwood.dev/alertx/about-us/', verify=False)
+if about_us_page.status_code == 200:
+    print("Passed")
+else:
+    print("Failed - about us page down")
+
+sample_product_page = requests.get('https://www.colinwood.dev/alertx/sample-product/', verify=False)
+if sample_product_page.status_code == 200:
+    print("Passed")
+else:
+    print("Failed - sample product page down")
+
